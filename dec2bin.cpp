6727012ea3a369a4594c289b797a857ef94b06b6
Aniket404 <aniketpatil99999@gmail.com>
2023-09-12 13:30:55 -0700

    first commit

--- dec2bin.cpp
+++ dec2bin.cpp
@@ -1,25 +1,41 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
-#include <math.h>
+#include<math.h>
 
-int main(){
-    int n ;
-    cin>>n;
+int main() {
+    // Write C++ code here
+    int n, ans = 0, i = 0;
+    cin >> n;
 
-    int ans=0;
-    int i=0;
-    while(n != 0){
+    int num = 0;
+    if(n<0){        
+        num = -n;
         
-        int bit = n & 1;
+        while(num != 0 ) {
 
-        ans = (bit* pow(10,i) ) + ans ;
+            int bit  = num & 1;
+            ans = (bit * pow(10, i) ) + ans;
+            num = num >> 1;
+        i++;
+        }
+        
+    cout<<" Answer is - "<< ans << endl;
+}
+
+
+else{       // For +ve decimal number.
+
+    while(n != 0 ){
+
+        int bit  = n & 1;
+        ans = (bit * pow(10, i) ) + ans;
         n = n >> 1;
         i++;
     }
-    cout<<"Answer is "<< ans <<endl;
+    cout<<" Answer is "<< ans << endl;
     
-    
-    
+}
 
-    
+
 }
