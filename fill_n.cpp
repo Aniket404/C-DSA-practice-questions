af756082df136769377e868b3647f49adc1eb3fd
Aniket Patil <63067583+Aniket404@users.noreply.github.com>
2023-09-19 13:50:51 -0700

    fill_n.cpp

--- fill_n.cpp
+++ fill_n.cpp
@@ -3,7 +3,7 @@
  int main()
  {
     int ar[10];
-    fill_n(ar,10,3);
+    fill_n(ar,10,3); //used to put same values in every location
     for (int i = 0; i <= 10; i++)
     {
      cout << ar[i] << endl;
