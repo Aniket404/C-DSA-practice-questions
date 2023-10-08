6fad1bbc1efa5d5c3127cba3eb3928192eb62473
Aniket404 <aniketpatil99999@gmail.com>
2023-10-08 10:00:28 -0700

    first commit

--- primefunc.cpp
+++ primefunc.cpp
@@ -4,7 +4,7 @@ using namespace std;
 bool isPrime(int n){
     for(int i=2; i<n;i++){
         if(n%i == 0){
-            //cout<<"is a Prime number"<<endl;
+            //cout<<"is a Prime number"<<endl<<endl;
           return 0;
         }
     }
