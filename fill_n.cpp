efda955a1ae83754443f11abb6fdd01eed79084c
Aniket404 <aniketpatil99999@gmail.com>
2023-10-09 23:01:12 -0700

    first commit

--- fill_n.cpp
+++ fill_n.cpp
@@ -6,6 +6,6 @@
     fill_n(ar,10,3); //used to put same values in every location
     for (int i = 0; i <= 10; i++)
     {
-     cout << ar[i] << endl;
+     cout << ar[i] << endl<< endl;
     }
   }
