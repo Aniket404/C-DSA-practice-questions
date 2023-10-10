05d2a7f02ad6a535a1398447c35a25d3dfdc6e1c
Aniket404 <aniketpatil99999@gmail.com>
2023-10-09 22:58:18 -0700

    first commit

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -8,7 +8,7 @@ int main(){
     cout<<a<<endl<<b<<endl;
     for(int i=1;i<=n;i++){
         int nextNumber=a+b;
-        cout<<nextNumber<<endl;
+        cout<<nextNumber<<endl<<endl;
         a=b;
         b=nextNumber;
 
