eeb52279af173db0b943339d9cdc38ea4d3b7226
Aniket Patil <63067583+Aniket404@users.noreply.github.com>
2024-06-29 20:16:46 -0700

    Update peak_index_in_an_array.cpp

--- peak_index_in_an_array.cpp
+++ peak_index_in_an_array.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
+        //store and compute method
+     int s=0;
+        int e=arr.size()-1;
+        int mid=s+(e-s)/2;
+        int ansIndex=-1;
+        while(s<=e){
+            if(arr[mid]<arr[mid+1]){
+                s=mid+1;
+            }
+            else{
+                ansIndex=mid;
+                e=mid-1;
+            }
+            mid=s+(e-s)/2;
+        }
+        return ansIndex;
+        
+    }
+};
+method 2
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
         int s=0;
         int e=arr.size()-1;
         int mid=s+(e-s)/2;
